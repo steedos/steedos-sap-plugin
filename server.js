1457e774c746edb7429fb82b66eb71dfac0d08a9
sunhaolin <sunhaolin@hotoa.com>
2019-06-24 18:09:08 +0800

    接口开发

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ var objectql = require("@steedos/objectql");
 var path = require('path');
 var schedule = require('node-schedule');
 // 项目代码
-const ZMM_PO_AFNAM = require('./lib/ZMM_PO_AFNAM');
+const ZMM_PO_AFNAM = require('./lib/sap_api/ZMM_PO_AFNAM');
 // 供应商
-const ZMM_PO_LIFNR = require('./lib/ZMM_PO_LIFNR');
+const ZMM_PO_LIFNR = require('./lib/sap_api/ZMM_PO_LIFNR');
 // 物料组
-const ZMM_PO_MATKL = require('./lib/ZMM_PO_MATKL');
+const ZMM_PO_MATKL = require('./lib/sap_api/ZMM_PO_MATKL');
+// WBS元素
+const ZMM_PO_POSID = require('./lib/sap_api/ZMM_PO_POSID');
+// 作业编号
+const ZMM_PO_ASNUM = require('./lib/sap_api/ZMM_PO_ASNUM');
 // 获取用款单
-const ZMM_ZOBJNR_GET = require('./lib/ZMM_ZOBJNR_GET');
+const ZMM_ZOBJNR_GET = require('./lib/sap_api/ZMM_ZOBJNR_GET');
 // 获取PR行项目,物料
-const Z_BAPI_REQUISITION_GETITEMSGW = require('./lib/Z_BAPI_REQUISITION_GETITEMSGW');
+const Z_BAPI_REQUISITION_GETITEMSGW = require('./lib/sap_api/Z_BAPI_REQUISITION_GETITEMSGW');
 server.Fiber(function () {
     server.Profile.run("Server startup", function () {
         server.loadServerBundles();
@@ -25,12 +29,11 @@ server.Fiber(function () {
                 Creator.Apps[app._id] = app
             })
 
-            console.log('uuflowManager.create_instance: ', uuflowManager.create_instance);
 
-            // 自定义业务逻辑
             let steedosSchema = objectql.getSteedosSchema();
             let abapSystem = Meteor.settings.plugins.sap.abapConfig;
             let spaceId = Meteor.settings.plugins.sap.spaceId;
+            let flows = Meteor.settings.plugins.sap.flows;
 
             // 定时执行同步
             let sap_sync_rule = Meteor.settings.cron.sap_sync_rule;
@@ -46,6 +49,10 @@ server.Fiber(function () {
                     ZMM_PO_LIFNR.run(abapSystem, steedosSchema, spaceId);
                     // 获取物料组
                     ZMM_PO_MATKL.run(abapSystem, steedosSchema, spaceId);
+                    // 获取WBS元素
+                    ZMM_PO_POSID.run(abapSystem, steedosSchema, spaceId);
+                    // 获取作业编号
+                    ZMM_PO_ASNUM.run(abapSystem, steedosSchema, spaceId);
 
                     console.timeEnd('sap_sync_rule');
                 }, function () {
@@ -59,9 +66,18 @@ server.Fiber(function () {
                 schedule.scheduleJob(sap_get_draft_rule, Meteor.bindEnvironment(function () {
                     console.time('sap_get_draft_rule');
                     // 获取用款单
-                    ZMM_ZOBJNR_GET.run(abapSystem, steedosSchema, spaceId)
+                    if (flows.yongkuandan) {
+                        ZMM_ZOBJNR_GET.run(abapSystem, spaceId, flows.yongkuandan);
+                    } else {
+                        console.error('server: flows.yongkuandan is null')
+                    }
+
                     // 获取PR行项目，物料
-                    Z_BAPI_REQUISITION_GETITEMSGW.run(abapSystem, steedosSchema, spaceId)
+                    if (flows.wuliao) {
+                        Z_BAPI_REQUISITION_GETITEMSGW.run(abapSystem, spaceId, flows.wuliao);
+                    } else {
+                        console.error('server: flows.wuliao is null')
+                    }
 
                     console.timeEnd('sap_get_draft_rule');
                 }, function () {
