42b4471100b155476fa283bd69e7d2fdcd760bda
sunhaolin <sunhaolin@hotoa.com>
2019-07-05 13:01:55 +0800

    物资申购 状态回传及刷新数据按钮

--- flow_scripts/服务合同会签流程.js
+++ flow_scripts/服务合同会签流程.js
@@ -1,39 +1,43 @@
 // 生成采购订单
-$("[name='EBELN']").parent().append('<button id="getOrderIdBtn" type="button" >生成采购订单</button>');
+var ins = Workflow.getInstance();
+if (ins.current_step_name === '生成采购单号') {
+    $("[name='EBELN']").parent().append('<button id="getOrderIdBtn" type="button" >生成采购单号</button>');
 
-$("#getOrderIdBtn").on('click', function (e) {
-    console.log('getOrderIdBtn click！');
-    var url = `${Meteor.settings.public.webservices.creator.url}/${Session.get("spaceId")}/draft_purchase/${Session.get("instanceId")}`;
-    var data = {};
-    data = JSON.stringify(data);
-    $(document.body).addClass("loading");
+    $("#getOrderIdBtn").on('click', function (e) {
+        console.log('getOrderIdBtn click！');
+        var url = `${Meteor.settings.public.webservices.creator.url}/${Session.get("spaceId")}/draft_purchase/${Session.get("instanceId")}`;
+        var data = {};
+        data = JSON.stringify(data);
+        $(document.body).addClass("loading");
 
-    $.ajax({
-        url: url,
-        type: "POST",
-        async: true,
-        data: data,
-        dataType: "json",
-        processData: false,
-        contentType: "application/json",
+        $.ajax({
+            url: url,
+            type: "POST",
+            async: true,
+            data: data,
+            dataType: "json",
+            processData: false,
+            contentType: "application/json",
 
-        success: function (responseText, status) {
-            $(document.body).removeClass("loading");
-            if (!responseText.ok) {
-                toastr.error(responseText.msg);
-                return;
+            success: function (responseText, status) {
+                $(document.body).removeClass("loading");
+                if (!responseText.ok) {
+                    toastr.error(responseText.msg);
+                    return;
+                }
+
+                toastr.success('生成采购单号成功！');
+            },
+            error: function (xhr, msg, ex) {
+                $(document.body).removeClass("loading");
+                toastr.error(msg);
             }
-
-            toastr.success('生成采购订单成功！');
-        },
-        error: function (xhr, msg, ex) {
-            $(document.body).removeClass("loading");
-            toastr.error(msg);
-        }
+        })
     })
-})
+}
+
 
 // 科目分配类别： 值为P(项目(服务采购)(无估价))的时候 WBS必填，其余情况下WBS不为必填
 $(".instance-form").on('instance-before-submit', function (e) {
-// TODO
+    // TODO
 })
--- flow_scripts/物资申购流程.js
+++ flow_scripts/物资申购流程.js
@@ -1,11 +1,11 @@
 // 刷新数据
 var ins = Workflow.getInstance();
-const yongkuanFlowId = '468b4caded61e2a13b89027c';
-if (ins.flow === yongkuanFlowId && (Session.get('box') === 'inbox' || Session.get('box') === 'draft') && ins.current_step_name === '发起审批' && ins.applicant === Meteor.userId()) {
+const wuliaoFlowId = '73c38583e4afff48aba584c6';
+if (ins.flow === wuliaoFlowId && (Session.get('box') === 'inbox' || Session.get('box') === 'draft') && ins.current_step_name === '开始' && ins.applicant === Meteor.userId()) {
     $(".btn-workflow-chart").parent().append('<button class="btn btn-default btn-workflow-syncSAPData">刷新数据</button>');
     $(".btn-workflow-syncSAPData").on('click', function () {
         console.log('刷新数据！');
-        var url = `${Meteor.settings.public.webservices.creator.url}/${Session.get("spaceId")}/yongkuan/values/${Session.get("instanceId")}`;
+        var url = `${Meteor.settings.public.webservices.creator.url}/${Session.get("spaceId")}/wuliao/values/${Session.get("instanceId")}`;
         $(document.body).addClass("loading");
         $.ajax({
             url: url,
